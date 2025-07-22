1f47a9df9d01fb95497f54fba9ecf06aa7bda641
sameerkeshvani <sameeekeshvani@gmail.com>
2025-07-21 21:59:53 -0400

    Updated

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "../api/auth/authOptions";
 import clientPromise from "@/lib/mongodb";
 import { redirect } from "next/navigation";
 
